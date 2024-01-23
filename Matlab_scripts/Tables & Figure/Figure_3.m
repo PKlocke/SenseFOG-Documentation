3ea875d030329c6c81430ff17bcb2849e17ed1cb
PKlocke <152193415+PKlocke@users.noreply.github.com>
2024-01-23 23:26:33 +0100

    Update Figure_3.m

--- Matlab_scripts/Tables & Figure/Figure_3.m
+++ Matlab_scripts/Tables & Figure/Figure_3.m
@@ -185,9 +185,12 @@ for t = 1:length(filenames)
 end
 
 filepath        = subjectdata.generalpath; cd(filepath)
-filepath        = extractBefore(filepath,"/Time-Frequency-Data");   
-filepath        = append(filepath, "/Table & Figures"); cd(filepath)
+filepath        = extractBefore(filepath,"Time-Frequency-Data");   
+filepath        = char(filepath);
+filepath        = string(filepath(1:end-1));
+filepath        = append(filepath,filesep, "Table & Figures"); cd(filepath)
 save([sprintf('%s.mat',"Figure_3")],"Sensefog_ResultsTable");
+
 
 %% Plot Figure 3
 addpath("customcolormap")
