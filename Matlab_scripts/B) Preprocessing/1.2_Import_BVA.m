1da94a03562eea84358c0a39256d8aff45feef3e
PKlocke <152193415+PKlocke@users.noreply.github.com>
2024-01-30 16:07:09 +0100

    Add files via upload

---
+++ Matlab_scripts/B) Preprocessing/1.2_Import_BVA.m
@@ -0,0 +1,70 @@
+%%=====  Import_BVA.m ========================================%
+%Original author(s): Philipp Klocke, Moritz Loeffler
+
+%This script will import the original raw data of specified EEG data and save the data to a pre-determined filepath. 
+%==============================================================================
+close all
+
+EEG_File            = struct;                                            % Create a file structure
+[EEG_file,path]     = uigetfile('*.eeg');                                % Choose the desired EEG File
+cd(path)
+EEG_File.filename   = strrep(EEG_file,'.eeg','');                        % Creates the filename by cutting off the suffix (.eeg or .vmrk or .vhdr)
+EEG_File.eeg        = [EEG_File.filename,'.eeg'];                        % Read the data, header, and marker file.
+EEG_File.vhdr       = ft_read_header([EEG_File.filename,'.vhdr']);
+EEG_File.vmrk       = ft_read_event([EEG_File.filename,'.vmrk']);
+EEG_File.fs_eeg     = EEG_File.vhdr.Fs;
+dat.dataset         = ft_read_data(EEG_File.eeg,'header', EEG_File.vhdr);
+dat.label           = EEG_File.vhdr.label;
+
+findTrial           = [];
+findTime            = [];
+
+for n = 1:length(EEG_File.vmrk)
+    t = EEG_File.vmrk(n).type;                                            % Examine the marker file to find the length of each trial
+    if strcmp(t,'New Segment')                                            % and the start/end time.
+      findTrial = [findTrial;EEG_File.vmrk(n).sample]; 
+    end
+    if strcmp(t,'Time 0')
+      findTime = [findTime;EEG_File.vmrk(n).sample]; 
+    end    
+end
+
+if length(findTrial) == 1
+   trialLength = size(dat.dataset,2);                                      % Check if the data is 1 trial long (ERP), or greater than 1 trial long  
+   numTrial = 1;                                                           % (time-frequency analyses).
+else                                                                        
+   trialLength = findTrial(2) - findTrial(1);  
+   numTrial = size(dat.dataset,2)/trialLength; 
+end
+
+
+StartTime           = -(0 - 1)/EEG_File.vhdr.Fs;
+EndTime             = StartTime + (trialLength/EEG_File.vhdr.Fs); 
+
+ftTime              = [];
+ftTrial             = [];
+EEG_File.eegtime    = StartTime:(1/EEG_File.vhdr.Fs):(EndTime - (1/EEG_File.vhdr.Fs)); 
+
+
+for t = 1:numTrial
+    trl = dat.dataset(:,(1 + trialLength*(t-1)):trialLength*t);             % Match the time and trials with Fieldtrip's format
+    ftTrial{1,t} = trl;  
+    ftTime{1,t} = EEG_File.eegtime;
+end
+                                                                            % Create a data structure with all the relevant fields
+dat.time           = ftTime;
+dat.trial          = ftTrial;
+data               = ft_preprocessing([], dat);
+
+EEG_File.dat       = dat;
+EEG_File.data      = data;
+EEG_File.channels  = cell2table(EEG_File.vhdr.label);                       % Creates a list of all EEG Channel electrode names
+
+
+%Clean-UP
+clear EndTime findTime ftTime ftTrial n numTrial StartTime t trialLength trl dat data EEG_file findTrial
+
+%Save DATA
+save([path EEG_File.filename '_raw.mat'], 'EEG_File', '-mat')       
+
+% *********************** END OF SCRIPT ************************************************************************************************************************
---
+++ Matlab_scripts/B) Preprocessing/1.5_Data_Alignment.m
@@ -0,0 +1,215 @@
+%%=====  Data_Alignment.m ========================================%
+
+%Date: November 2023
+%Original author(s): Philipp Klocke, Moritz Loeffler
+
+%This script will help align both EEG and LFP and IMU data. As such, the M1 sequence will be 
+%traced in the EEG signal which is the mutual timepoint where IMU and EEG data are synchronized 
+%(automatic). We will first cut the EEG signal before IMU recording started.In a second step we 
+%will manually identify the stimulation train of 1mA, 125 Hz and 100µs which is delivered at the
+%beginning of each LFP/EEG recording. The stimulation offset is followed by a sharp transition  
+%artefact visible in all different time series and occurs a few miliseconds after the stimulation  
+%train ends.We will be using this sharp transition artefact to align the different time series in time.
+%The prespecified timepoints where the transition artefact occurs have been set manually by the authors 
+%already and are stored in the the subject's individual datafile (e.g. see sub-01-datafile.m). 
+%The files will be aligned according to the artefact and redundant traces cut out. The data will
+%be stored in a pre-specified filepath. See the supplementary materials for more information.
+
+%===================================================================%
+
+%Load EEG and LFP data and IMU data by specifying subject and SESSION of interest
+clear; close all
+subjectdata.generalpath         = uigetdir;                                                        % Example: SenseFOG-main/sub-XX/ses-standing
+cd(subjectdata.generalpath)
+filename                        = extractAfter(subjectdata.generalpath, "-main");                  % Create the specified filename
+filename                        = extractBefore(filename(2:end), "ses-");                          % Create the specified taskname
+filename                        = filename(1:end-1); 
+taskname                        = extractAfter(subjectdata.generalpath,"ses-");                    % Create the specified taskname
+taskname                        = append("ses-", taskname);                                        % Create the specified taskname
+filepath                        = extractBefore(subjectdata.generalpath, "ses");                   % Create the specified filepath
+filepath                        = filepath(1:end-1); 
+fullname                        = append(subjectdata.generalpath, filesep, "ieeg", filesep,filename, "-", taskname, "_raw.mat"); 
+
+if isfile(fullname); load(fullname); end                                                            % LOAD JSON [LFP] FILE
+fullname                        = append(subjectdata.generalpath,filesep, "eeg", filesep,filename, "-", taskname, "_raw.mat"); 
+if isfile(fullname); load(fullname); end                                                            % LOAD BVA [EEG] FILE
+fullname                        = append(subjectdata.generalpath,filesep, "motion", filesep, filename, "-", taskname, "_raw.mat");
+if isfile(fullname); load(fullname); end                                                            % LOAD HDF [IMU] FILE
+fullname                        = append(filepath, filesep, filename, "_datafile.mat"); load(fullname);
+
+subjectdata.filepath            = filepath;                                                         % Create the specified filepath
+subjectdata.fs_eeg              = 1000;                                                             % EEG sampling rate is 1000 Hz per default
+
+
+%=====LFP DATA =========================================================================
+%Specify time range for display using min and max values
+%Make sure the Stimulation artefact is present and that the artefact is aligned in both LFP traces 
+
+minx     = 0; %arbitrary value
+maxx     = 40000; %arbitrary value
+
+clc
+figure(1);
+    ax3 = subplot(2,1,1);
+    plot(1:length(LFP.interp_LFP_right),LFP.interp_LFP_right, 'LineWidth',1,'Color','r')
+    xlim([minx maxx])
+    xlabel('Samples')
+    ylabel('Amplitude [a.u.]')
+    legend('Right LFP')
+    
+    ax4 = subplot(2,1,2);
+    plot(1:length(LFP.interp_LFP_left),LFP.interp_LFP_left, 'LineWidth',1,'Color','b')
+    xlim([minx maxx])
+    xlabel('Samples')
+    ylabel('Amplitude [a.u.]')
+    legend('Left LFP')
+    set(gcf,'color','white')
+    linkaxes([ax3,ax4],'xy');
+    sgtitle({'LFP Datasets'; sprintf('Trial: %s ', EEG_File.eeg)})
+    clc
+    
+%=== CLEAN-UP ====
+clear ans ax1 ax2 ax3 ax4 minx maxx
+
+%==== EEG DATA =========================================================================
+%Finding the M1-Sequence of the IMU Dataset that appears in the EEG dataset
+%The M1 Sequence represents the start of IMU recording in the EEG dataset
+if      exist("IMU_data")
+        M1                      = struct2table(EEG_File.vmrk);
+        index_M1                = find(strcmp('M  1',(M1{:,2}))); %Finding the index of where M  1 appears
+        sample_M1               = table2array(M1(index_M1,"sample"));
+else    sample_M1               = 1; end
+
+
+%Plot EEG Raw Data (single channels):
+figure(2);
+    plot(1:length(EEG_File.dat.dataset), EEG_File.dat.dataset(end,:),'-v','MarkerIndices',[sample_M1(1)],'MarkerFaceColor','red')
+    text(sample_M1(1,1),1000,'M1')
+    xlabel('Samples'); ylabel('arbitrary unit')
+    xline(sample_M1(1), '--r', 'LineWidth',2)
+    xlim([sample_M1(1)-2500 sample_M1(1)+2500])
+    set(gcf,'color','white')
+    title('Finding M1 Sequence')
+
+%=== CLEAN-UP ====
+clear numTrial index_M1 M1 
+
+
+%==== ALIGNING EEG AND IMU DATA =============================================================================
+%Next, we will align EEG and IMU data accoring to the M1-Sequence and cut redundant EEG data.
+EEG_pre_signal          = EEG_File.dat.dataset(:,(sample_M1(1,1):end));
+EEG_File.eegtime_new    = (1/subjectdata.fs_eeg):(1/subjectdata.fs_eeg):(length(EEG_pre_signal)/subjectdata.fs_eeg);
+
+
+% === Visualizing NOT-ALIGNED signals for demonstration purposes ===
+figure(3)
+    ax1 = subplot(3,1,1)
+    plot(1:length(LFP.interp_LFP_right),LFP.interp_LFP_right, 'LineWidth',1,'Color','r')
+    xlabel('Samples'); xlim([0 20000])
+    title('Right LFP Signal')
+
+    ax2 = subplot(3,1,2)
+    plot(1:length(EEG_pre_signal), EEG_pre_signal(end,:))
+    xlabel('Samples')
+    xlim([0 20000]); ylim([-100 5000])
+    title('EEG Data - IPG')
+    
+    ax3 = subplot(3,1,3)
+    plot(1:length(EEG_pre_signal), EEG_pre_signal(14,:))
+    xlim([0 20000])
+    title('EEG Data - C4')
+    linkaxes([ax2, ax3],'x')
+    set(gcf,'color','white')
+    sgtitle('Non-Aligned Datasets')
+    clear ax1 ax2 ax3 
+
+
+% Specify Tasknames
+if     taskname == 'ses-standing';  task = 'Stand';
+elseif taskname == 'ses-sitting';   task = 'Sit'; 
+elseif taskname == 'ses-walk';      task = 'Walk';
+elseif taskname == 'ses-walkws';    task = 'WalkWS';
+elseif taskname == 'ses-walkint';   task = 'WalkINT';
+elseif taskname == 'ses-walkint2';  task = 'WalkINT_new';
+end
+
+%Align files by cutting off non-overlapping traces
+%The code will call the hardcoded timepoints of the sharp transition artefact and the delay between the transition artefacts of EEG and LFP traces. 
+%Based on the delay, the time series with redundant data will be cut according to the delay. This is not an automatic process but requires the prior
+%manual addition of the timepoints in the subject's datafile, as has been done by the author team already. 
+if subjectdata.signalpoint.(task).LFP_signal > subjectdata.signalpoint.(task).EEG_signal
+   LFP_signal_R                         = LFP.interp_LFP_right(:,[subjectdata.signalpoint.(task).delay:end]);                                           % Cut LFP file according to time-delay (RIGHT)
+   LFP_signal_L                         = LFP.interp_LFP_left(:,[subjectdata.signalpoint.(task).delay:end]);                                            % Cut LFP file according to time-delay (LEFT)
+   EEG_File.dat.dataset_new             = EEG_pre_signal;
+   EEG_signal                           = EEG_pre_signal; 
+
+elseif subjectdata.signalpoint.(task).EEG_signal > subjectdata.signalpoint.(task).LFP_signal
+   EEG_File.dat.dataset_new             = EEG_pre_signal(:,[subjectdata.signalpoint.(task).delay:end]);                                                 % Cut EEG file according to time-delay
+   EEG_File.eegtime_new                 = (1/subjectdata.fs_eeg):(1/subjectdata.fs_eeg):(length(EEG_File.dat.dataset_new)/subjectdata.fs_eeg);
+   %EEG_File.eegtime_new                = EEG_File.eegtime_new(:, [subjectdata.signalpoint.(task).delay:end]);                                          % Cut EEG time file according to time-delay 
+
+   if exist("IMU_data")
+   for i = 1:3 
+        IMU_data.interp_accelerometer(i).Sensor = IMU_data.interp_accelerometer(i).Sensor([subjectdata.signalpoint.(task).delay:end],:);                % Prepare IMU files
+        IMU_data.interp_gyroscope(i).Sensor     = IMU_data.interp_gyroscope(i).Sensor([subjectdata.signalpoint.(task).delay:end],:);                    % Prepare IMU files
+        IMU_data.interp_magnetometer(i).Sensor  = IMU_data.interp_magnetometer(i).Sensor([subjectdata.signalpoint.(task).delay:end],:);                 % Prepare IMU files
+    end
+
+    IMU_data.imutime                     = (1/subjectdata.fs_eeg):(1/subjectdata.fs_eeg):(length(IMU_data.interp_accelerometer(1).Sensor)/subjectdata.fs_eeg); 
+    end
+   
+   EEG_signal                           = EEG_File.dat.dataset_new;
+   LFP_signal_R                         = LFP.interp_LFP_right; 
+   LFP_signal_L                         = LFP.interp_LFP_left;
+end
+
+
+% VISUALIZE ALIGNED DATASETS
+xstart                                  = subjectdata.signalpoint.(task).LFP_signal - subjectdata.signalpoint.(task).delay;
+
+figure(4)
+ax1 = subplot(3,1,1)
+    plot(1:length(LFP_signal_R),LFP_signal_R, 'LineWidth',1,'Color','r')
+    xlabel('Samples')
+    xlim([xstart-5000 xstart+5000])
+    legend('Right STN LFP','Box', 'off')
+    title('LFP Signal')
+    
+ax2 = subplot(3,1,2)
+    plot(1:length(EEG_signal), EEG_signal(end-1,:))
+    xlabel('Samples')
+    xlim([xstart-5000 xstart+5000])
+    ylim([-2000 2000])
+    legend('Impulse Generator [IPG]','Box', 'off')
+    title('EEG Data - IPG')
+
+    if exist("IMU_data")
+ax3 = subplot(3,1,3)
+    plot(1:length(IMU_data.interp_accelerometer(1).Sensor),IMU_data.interp_accelerometer(1).Sensor(:,1))
+    xlabel('Samples')
+    xlim([xstart-5000 xstart+5000])
+    legend('Accelerometer')
+    title('IMU')
+    else; ax3 = subplot(3,1,3); end
+    set(gcf,'color','white')
+    sgtitle({'Aligned LFP dataset and EEG dataset'; sprintf('Trial: %s ', EEG_File.eeg)})
+    linkaxes([ax1, ax2 ax3],'x')
+    clear ax1 ax2 ax3 xstart EEG_pre_signal 
+
+   
+%Assign alligned Files back to the original file
+LFP.LFP_signal_R                = LFP_signal_R;
+LFP.LFP_signal_L                = LFP_signal_L;
+LFP.LFP_time                    = (1/subjectdata.fs_eeg):(1/subjectdata.fs_eeg):(length(LFP_signal_R)/subjectdata.fs_eeg);
+EEG_File.EEG_signal             = EEG_signal;
+EEG_File.EEG_time               = (1/subjectdata.fs_eeg):(1/subjectdata.fs_eeg):(length(EEG_File.eegtime_new)/subjectdata.fs_eeg);
+EEG_File                        = rmfield(EEG_File, {'eegtime'; 'dat'; 'data'; 'eegtime_new'});
+LFP                             = rmfield(LFP, {'raw_lfp_left'; 'raw_lfp_right'; 'fs_lfp'; 'interp_LFP_right'; 'interp_LFP_left' });
+if exist("IMU_data"); IMU_data  = rmfield(IMU_data, {'info'});end
+
+
+%========================SAVING DATA ================================
+if exist("IMU_data"); save([subjectdata.filepath filesep char(taskname) filesep 'motion' filesep filename '-' char(taskname) '_gaitalg.mat'], 'IMU_data', '-mat'); end
+if exist("LFP");      save([subjectdata.filepath filesep char(taskname) filesep 'ieeg' filesep filename '-' char(taskname) '_lfpalg.mat'], 'LFP', '-mat'); end     
+if exist("EEG_File"); save([subjectdata.filepath filesep char(taskname) filesep 'eeg' filesep filename '-' char(taskname) '_eegalg.mat'], 'EEG_File', '-mat'); end
+% *********************** END OF SCRIPT ************************************************************************************************************************
